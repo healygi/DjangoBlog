ac586cde565fb7634e92bff73b3bba07b2142cb7
healygi <healygi@tcd.ie>
2022-06-29 15:13:10 +0000

    link up urls and templates

--- blog/admin.py
+++ blog/admin.py
@@ -13,4 +13,11 @@ class PostAdmin(SummernoteModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    
+
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email address', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
