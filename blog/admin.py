7b34db6ae3c3090172043942b304827df710d156
healygi <healygi@tcd.ie>
2022-06-29 11:02:36 +0000

    push before closing

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    summernote_fields = ('content')
+    search_fields = ['title', 'content']
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
